b0f431ea3f1fadfe5c2441c8f376326e3bae3eb0
Justica <atsuronreplaced@gmail.com>
2023-12-23 00:17:30 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# RendererToyModelCs
+This is a ported version of [renderer_toy_model](https://github.com/atsuhiron/renderer_toy_model) to C#.
---
+++ RendererToyModelCs/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RendererToyModelCs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
