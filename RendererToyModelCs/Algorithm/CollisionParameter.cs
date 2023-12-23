960da2b32cd6c5cc841c5cb7812a506f78633b4d
atsuhiron <atsuronreplaced@gmail.com>
2023-12-23 20:04:25 +0900

    Add sample code, collision parameter

---
+++ RendererToyModelCs/Algorithm/CollisionParameter.cs
@@ -0,0 +1,9 @@
+﻿namespace RendererToyModelCs.Algorithm
+{
+    public record CollisionParameter
+    {
+        public float CoefA;
+        public float CoefB;
+        public float Dist;
+    }
+}
--- RendererToyModelCs/Program.cs
+++ RendererToyModelCs/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace RendererToyModelCs
+﻿using MathNet.Numerics.LinearAlgebra;
+
+namespace RendererToyModelCs
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var vec1 = Vector<float>.Build.DenseOfArray(new float[] { 0.0f, -9.1f, 0.2f });
+            var vec2 = Vector<float>.Build.DenseOfArray(new float[] { 0.3f, -1.1f, 4.1f });
+
+            Console.WriteLine(vec1);
+            Console.WriteLine(vec2);
+            Console.WriteLine(vec1 + vec2);
+            Console.WriteLine(vec1 - vec2);
+            Console.WriteLine(vec1 * vec2);
+            Console.WriteLine(vec1 / vec2);
         }
     }
 }
