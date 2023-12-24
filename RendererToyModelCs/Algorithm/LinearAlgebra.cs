6390dfb7de61def1e8510bf6224d0d81cc3e6c32
Justica <atsuronreplaced@gmail.com>
2023-12-24 14:02:49 +0900

    Add some static function about linear algebra

---
+++ RendererToyModelCs/Algorithm/LinearAlgebra.cs
@@ -0,0 +1,56 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+
+namespace RendererToyModelCs.Algorithm
+{
+    public static class LinearAlgebra
+    {
+        public static CollisionParameter CalcCollisionParam()
+        {
+            return null;
+        }
+
+        public static bool DoCollide(CollisionParameter cParam, Tuple<float, float> basisNorm)
+        {
+            if (cParam.CoefA < 0 || cParam.CoefB < 0)
+            {
+                return false;
+            }
+
+            float a = cParam.CoefA / basisNorm.Item1;
+            float b = cParam.CoefA / basisNorm.Item2;
+            
+            if ((a + b) > 0.5)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static Vector<float> RotateVector(in Vector<float> vector, in Vector<float> normalizedAxial, float radian)
+        {
+            float mcos = 1 - MathF.Cos(radian);
+            float cos = MathF.Cos(radian);
+            float sin = MathF.Sin(radian);
+            var nx = normalizedAxial.ElementAt(0);
+            var ny = normalizedAxial.ElementAt(1);
+            var nz = normalizedAxial.ElementAt(2);
+
+            Matrix<float> r = Matrix<float>.Build.Dense(3, 3, 
+                [
+                    nx * nx * mcos + cos,
+                    nx * ny * mcos + nz * sin,
+                    nz * nx * mcos - ny * sin,
+                    
+                    nx * ny * mcos - nz * sin,
+                    ny * ny * mcos + cos,
+                    ny * nz * mcos + nx * sin,
+                    
+                    nz * nx * mcos + ny * sin,
+                    ny * nz * mcos - nx * sin,
+                    nz * nz * mcos + cos,
+                ]);
+
+            return r * vector;
+        }
+    }
+}
--- RendererToyModelCs/Program.cs
+++ RendererToyModelCs/Program.cs
@@ -1,4 +1,6 @@
-﻿using MathNet.Numerics.LinearAlgebra;
+﻿using System;
+using MathNet.Numerics.LinearAlgebra;
+using RendererToyModelCs.Algorithm;
 
 namespace RendererToyModelCs
 {
@@ -6,8 +8,8 @@ namespace RendererToyModelCs
     {
         static void Main(string[] args)
         {
-            var vec1 = Vector<float>.Build.DenseOfArray(new float[] { 0.0f, -9.1f, 0.2f });
-            var vec2 = Vector<float>.Build.DenseOfArray(new float[] { 0.3f, -1.1f, 4.1f });
+            var vec1 = Vector<float>.Build.DenseOfArray([0.0f, -9.1f, 0.2f]);
+            var vec2 = Vector<float>.Build.DenseOfArray([0.3f, -1.1f, 4.1f]);
 
             Console.WriteLine(vec1);
             Console.WriteLine(vec2);
@@ -15,6 +17,17 @@ namespace RendererToyModelCs
             Console.WriteLine(vec1 - vec2);
             Console.WriteLine(vec1 * vec2);
             Console.WriteLine(vec1 / vec2);
+
+            var vec3 = Vector<float>.Build.Dense(3, 1f);
+            Console.WriteLine(vec3);
+
+            var norm1 = Vector<float>.Build.DenseOfArray([1f, 0f, 0f]);
+            var norm2 = Vector<float>.Build.DenseOfArray([0f, 1f, 0f]);
+            var norm3 = Vector<float>.Build.DenseOfArray([0f, 0f, 1f]);
+
+            Console.WriteLine(LinearAlgebra.RotateVector(vec3, norm1, MathF.PI / 4));
+            Console.WriteLine(LinearAlgebra.RotateVector(vec3, norm2, MathF.PI / 4));
+            Console.WriteLine(LinearAlgebra.RotateVector(vec3, norm3, MathF.PI / 4));
         }
     }
 }
