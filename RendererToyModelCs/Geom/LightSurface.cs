00f9f92ccfe16aa781321f160fe00f6009727191
atsuhiron <atsuronreplaced@gmail.com>
2024-01-02 11:46:16 +0900

    Add LightSurface

---
+++ RendererToyModelCs/Geom/LightSurface.cs
@@ -0,0 +1,21 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+using RendererToyModelCs.Algorithm;
+using RendererToyModelCs.Chromatic;
+
+namespace RendererToyModelCs.Geom
+{
+    public class LightSurface(List<Vector<float>> points, string name, CLight light) : BaseSurface(points, name)
+    {
+        public override SurfaceType SufType => SurfaceType.Light;
+
+        public CLight Light { get; init; } = light;
+
+        public override List<IParticle> GetCollisionParticle(in IParticle inPaticle, in CollisionParameter cParam, int num)
+        {
+            return new List<IParticle>
+            {
+                Particle.CreateTerminatedParticle(inPaticle, Light)
+            };
+        }
+    }
+}
