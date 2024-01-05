9cdae92bf43ab938f5624d630c0bdbdbf53d0fd5
Justica <atsuronreplaced@gmail.com>
2024-01-05 23:50:03 +0900

    Add World

---
+++ RendererToyModelCs/WorldObject/World.cs
@@ -0,0 +1,10 @@
+﻿using RendererToyModelCs.Geom;
+
+namespace RendererToyModelCs.WorldObject
+{
+    public class World(List<ISurface> surfaces, Camera camera)
+    {
+        public List<ISurface> Surfaces { get; init; } = surfaces;
+        public Camera Camera { get; init; } = camera;
+    }
+}
