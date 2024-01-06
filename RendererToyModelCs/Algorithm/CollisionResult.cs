c7ea19590aba48a37dd82ffefa96611e3a699061
Justica <atsuronreplaced@gmail.com>
2024-01-06 22:23:30 +0900

    Add record

---
+++ RendererToyModelCs/Algorithm/CollisionResult.cs
@@ -0,0 +1,19 @@
+﻿using RendererToyModelCs.Geom;
+
+namespace RendererToyModelCs.Algorithm
+{
+    public record CollisionResult
+    {
+        public CollisionParameter CollisionParame {  get; init; }
+
+        public ISurface? CollidedSurface { get; init; }
+
+        public CollisionResult(CollisionParameter param, ISurface? surface)
+        {
+            CollisionParame = param;
+            CollidedSurface = surface;
+        }
+
+        public bool GoThroughWorld() => CollisionParame == null;
+    }
+}
---
+++ RendererToyModelCs/WorldObject/RenderingConfig.cs
@@ -0,0 +1,14 @@
+﻿namespace RendererToyModelCs.WorldObject
+{
+    public record RenderingConfig
+    {
+        public int MaxGen {  get; init; }
+        public int RoughSurfaceChildNum { get; init; }
+
+        public RenderingConfig(int maxGen, int roughSurfaceChildNum)
+        {
+            MaxGen = maxGen;
+            RoughSurfaceChildNum = roughSurfaceChildNum;
+        }
+    }
+}
