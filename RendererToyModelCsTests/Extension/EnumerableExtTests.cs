62c4bc37c34bf2670a6a99ff5f898a248445165b
Justica <atsuronreplaced@gmail.com>
2024-01-14 21:45:07 +0900

    Add extension test

---
+++ RendererToyModelCsTests/Extension/EnumerableExtTests.cs
@@ -0,0 +1,19 @@
+﻿using RendererToyModelCs.Extension;
+
+namespace RendererToyModelCsTests.Extension
+{
+    public class EnumerableExtTests
+    {
+        [Fact]
+        public void WhereNotNullTest()
+        {
+            IEnumerable<string?> list = new List<string?>() { "1", "2", null, "4" };
+            List<string> actual = list.WhereNotNull().ToList();
+
+            Assert.Equal(3, actual.Count);
+            Assert.NotNull(actual[0]);
+            Assert.NotNull(actual[1]);
+            Assert.NotNull(actual[2]);
+        }
+    }
+}
