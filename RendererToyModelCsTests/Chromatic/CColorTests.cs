82ef2e7a7de1cd1c6fb5c5f7a945825e216424e7
Justica <atsuronreplaced@gmail.com>
2024-01-16 23:10:02 +0900

    Add ToLightTest, ToColorTest.

---
+++ RendererToyModelCsTests/Chromatic/CColorTests.cs
@@ -0,0 +1,26 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+using RendererToyModelCs.Chromatic;
+
+namespace RendererToyModelCsTests.Chromatic
+{
+    public class CColorTests
+    {
+        public static IEnumerable<object[]> ToLightTestData()
+        {
+            yield return new float[][] { [1f, 1f, 1f], [0f, 0f, 0f] };
+            yield return new float[][] { [0f, 0f, 0f], [1f, 1f, 1f] };
+            yield return new float[][] { [1f, 0.5f, 0f], [0f, 0.5f, 1f] };
+        }
+
+        [Theory]
+        [MemberData(nameof(ToLightTestData))]
+        public void ToLightTest(float[] colorElement, float[] expectedElemet)
+        {
+            var color = new CColor(Vector<float>.Build.DenseOfArray(colorElement));
+            var actualElement = color.ToLight().Elements;
+
+            var expectedElement = Vector<float>.Build.DenseOfArray(expectedElemet);
+            Assert.Equal(expectedElement, actualElement);
+        }
+    }
+}
---
+++ RendererToyModelCsTests/Chromatic/CLightTests.cs
@@ -0,0 +1,26 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+using RendererToyModelCs.Chromatic;
+
+namespace RendererToyModelCsTests.Chromatic
+{
+    public class CLightTests
+    {
+        public static IEnumerable<object[]> ToColorTestData()
+        {
+            yield return new float[][] { [1f, 1f, 1f], [0f, 0f, 0f] };
+            yield return new float[][] { [0f, 0f, 0f], [1f, 1f, 1f] };
+            yield return new float[][] { [1f, 0.5f, 0f], [0f, 0.5f, 1f] };
+        }
+
+        [Theory]
+        [MemberData(nameof(ToColorTestData))]
+        public void ToColorTest(float[] colorElement, float[] expectedElemet)
+        {
+            var light = new CLight(Vector<float>.Build.DenseOfArray(colorElement));
+            var actualElement = light.ToColor().Elements;
+
+            var expectedElement = Vector<float>.Build.DenseOfArray(expectedElemet);
+            Assert.Equal(expectedElement, actualElement);
+        }
+    }
+}
