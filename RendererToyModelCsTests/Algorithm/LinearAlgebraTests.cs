b0344ba1310683a16e1d547e1629ee4bc359e7fe
Justica <atsuronreplaced@gmail.com>
2024-01-21 06:04:26 +0900

    Add RotateVectorTest

--- RendererToyModelCsTests/Algorithm/LinearAlgebraTests.cs
+++ RendererToyModelCsTests/Algorithm/LinearAlgebraTests.cs
@@ -7,6 +7,15 @@ namespace RendererToyModelCsTests.Algorithm
 {
     public class LinearAlgebraTests
     {
+        private static readonly float s_tol = 1e-7f;
+
+        private bool IsNearlyEqual(Vector<float> vec1, Vector<float> vec2)
+        {
+            var diff = (vec1 - vec2).PointwiseAbs();
+            return diff.ToList().All(x => x < s_tol);
+        }
+
+
         public static IEnumerable<object[]> CrossTestData()
         {
             yield return new float[][] { [2f, 0f, 0f], [0f, 1f, 0f], [0f, 0f, 2f] };
@@ -22,7 +31,7 @@ namespace RendererToyModelCsTests.Algorithm
             var expected = Vector<float>.Build.Dense(expectedEle);
             
             var actual = LinearAlgebra.Cross(vec1, vec2);
-            Assert.Equal(expected, actual);
+            Assert.True(IsNearlyEqual(expected, actual));
         }
 
         private static readonly float[] s_suf1Point1 = [0f, 0f, 0f];
@@ -78,6 +87,31 @@ namespace RendererToyModelCsTests.Algorithm
 
             bool actual = LinearAlgebra.DoCollide(cParam, basisNorm);
             Assert.Equal(expected, actual);
+        }
+
+        public static IEnumerable<object[]> RotateVectorTestData()
+        {
+            yield return new float[][] { [1f, 0f, 0f], [1f, 0f, 0f], [1f, 0f, 0f] };   // xx
+            yield return new float[][] { [1f, 0f, 0f], [0f, 1f, 0f], [0f, 0f, -1f] };  // xy
+            yield return new float[][] { [1f, 0f, 0f], [0f, 0f, 1f], [0f, 1f, 0f] };   // xz
+            yield return new float[][] { [0f, 1f, 0f], [1f, 0f, 0f], [0f, 0f, 1f] };   // yx
+            yield return new float[][] { [0f, 1f, 0f], [0f, 1f, 0f], [0f, 1f, 0f] };  // yy
+            yield return new float[][] { [0f, 1f, 0f], [0f, 0f, 1f], [-1f, 0f, 0f] };   // yz
+            yield return new float[][] { [0f, 0f, 1f], [1f, 0f, 0f], [0f, -1f, 0f] };   // zx
+            yield return new float[][] { [0f, 0f, 1f], [0f, 1f, 0f], [1f, 0f, 0f] };  // zy
+            yield return new float[][] { [0f, 0f, 1f], [0f, 0f, 1f], [0f, 0f, 1f] };   // zz
+        }
+
+        [Theory]
+        [MemberData(nameof(RotateVectorTestData))]
+        public void RotateVectorTest(float[] vecEle, float[] axialEle, float[] expectedEle)
+        {
+            var vec = Vector<float>.Build.DenseOfArray(vecEle);
+            var axis = Vector<float>.Build.DenseOfArray(axialEle);
+            var expected = Vector<float>.Build.DenseOfArray(expectedEle);
+
+            var actual = LinearAlgebra.RotateVector(vec, axis, MathF.PI/2);
+            Assert.True(IsNearlyEqual(expected, actual));
         }
     }
 }
