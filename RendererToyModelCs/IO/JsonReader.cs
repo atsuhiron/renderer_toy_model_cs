8ca9493ae54103d2a2bc29a621e254847e1c4308
Justica <atsuronreplaced@gmail.com>
2024-02-09 23:03:28 +0900

    Add JsonReader

---
+++ RendererToyModelCs/IO/JsonReader.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+using System.Text.Json;
+using RendererToyModelCs.WorldObject;
+
+namespace RendererToyModelCs.IO
+{
+    public class JsonReader
+    {
+        private static readonly Encoding encoding_s = Encoding.GetEncoding("UTF-8");
+        
+        public static JsonElement ReadFile(string filePath)
+        {
+            var jsonStr = File.ReadAllText(filePath);
+            var bytes = encoding_s.GetBytes(jsonStr);
+            var reader = new Utf8JsonReader(new ReadOnlySpan<byte>(bytes));
+            return JsonElement.ParseValue(ref reader);
+        }
+    }
+}
